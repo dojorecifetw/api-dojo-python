42b19b0076b3e55a7ab3e85914a8b1e3653832bf
Roselma Mendes <roselma.mendes@gmail.com>
2015-05-24 16:57:50 -0300

    Start writing the first test.

--- todo/addtodo/tests.py
+++ todo/addtodo/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class AddTodo(APITestCase):
+
+    def test_add_todo(self):
+        response = self.client('/v1/todos/', {'description': 'Start to work'})
+
+        self.assertEquals(status.HTTP_201_CREATED, response.status_code)
